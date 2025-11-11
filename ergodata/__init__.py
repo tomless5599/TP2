893b72aefa521fc05fa6421761568635fc1e4f97
Thomas Lessard <thomaszoune1999@gmail.com>
2025-11-11 14:49:38 -0500

    Improve heuristic extraction of ergonomics metrics

---
+++ ergodata/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities for extracting ergonomics metrics from documents."""
+
+from .extractor import ErgonomicDocumentExtractor, ExtractionResult
+
+__all__ = ["ErgonomicDocumentExtractor", "ExtractionResult"]
