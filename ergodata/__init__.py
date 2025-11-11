c48b3a8ef008047e481e55a82191a4970dd09472
Thomas Lessard <thomaszoune1999@gmail.com>
2025-11-11 16:19:24 -0500

    Delete ergodata directory

--- ergodata/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities for extracting ergonomics metrics from documents."""
-
-from .extractor import ErgonomicDocumentExtractor, ExtractionResult
-
-__all__ = ["ErgonomicDocumentExtractor", "ExtractionResult"]
